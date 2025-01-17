8182b40ec0105898cd8d5cd18f3c123dbfdcc889
Justin Hill <justinrichardhill@gmail.com>
2025-01-16 21:14:30 -0500

    Figuring things out

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import DictionarySelector from './components/dictionarySelector';
+let group = ["Dante Singh", "Ricardo Marmolejo", "Saif Shihadeh", "Justin Hill"];
+let messages = group.map(person => `${person}, `);
+
 
 function App() {
   return (
-    <div>
-      <h1 className="mb-4 text-2xl font-bold">Welcome to the Dictionary App</h1>
+    <main class="bg-slate-100 p-8 dark:bg-slate-800">
+      <h1 className="mb-4 text-2xl font-bold pt-6 space-y-4 text-sky-500 dark:text-sky-400">Welcome to the Dictionary App</h1>
+      <h3 class="text-slate-700 dark:text-slate-500">A collaboration between {messages}.</h3>
       <DictionarySelector />
-    </div>
+    </main>
   );
 }
 
